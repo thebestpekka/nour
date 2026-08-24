a416a139c1108353dd45112df26c8d6a84af1403
thebestpekka <orzly123@gmail.com>
2026-08-24 11:51:58 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // if you use images from next/image:
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
